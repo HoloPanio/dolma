ad3e62c824ba12231c277e01b72366c182082122
Jackson <jrobe3221@gmail.com>
2021-05-07 23:28:05 -0500

    Updated core method to return decoded and encoded data.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.kefler = void 0;
 var decode_1 = require("./lib/decode");
 var encode_1 = require("./lib/encode");
-function kefler() {
-    console.log('bruh');
+function kefler(values) {
+    return {
+        encoded: encode_1.encodeTokens(values !== null && values !== void 0 ? values : ""),
+        decoded: decode_1.decodeTokens(values !== null && values !== void 0 ? values : "")
+    };
 }
 exports.kefler = kefler;
 kefler['encode'] = encode_1.encodeTokens;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import { decodeTokens } from "./lib/decode";
 import { encodeTokens } from "./lib/encode";
+import { MessageToken, Unitoken } from "./util/types/token";
 
-export function kefler() {
-	console.log('bruh');
+interface RootMethodResponse {
+	encoded: MessageToken[],
+	decoded: string
+}
+
+export function kefler(values?: Array<Unitoken | MessageToken | string> | string): RootMethodResponse {
+	return {
+		encoded: encodeTokens(values ?? ""),
+		decoded: decodeTokens(values ?? "")
+	}
 }
 
 kefler['encode'] = encodeTokens;
