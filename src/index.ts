c4451db2db1be1a6ac5a31b4e4c9f4498abd6a8a
Jackson <jrobe3221@gmail.com>
2021-05-07 23:25:57 -0500

    Updated core method to return decoded and encoded data.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { decodeTokens } from "./lib/decode";
 import { encodeTokens } from "./lib/encode";
+import { MessageToken, Unitoken } from "./util/types/token";
 
-export function kefler() {
-	console.log('bruh');
+export function kefler(values?: Array<Unitoken | MessageToken | string> | string) {
+	return {
+		encoded: encodeTokens(values ?? ""),
+		decoded: decodeTokens(values ?? "")
+	}
 }
 
 kefler['encode'] = encodeTokens;
